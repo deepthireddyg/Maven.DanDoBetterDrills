938878872d5cbc1b668290b1814c592796386312
Deepthi Ganji <115901667+deepthireddyg@users.noreply.github.com>
2022-11-04 16:43:20 -0400

    completed

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -19,7 +19,7 @@ public class StringUtilities {
      */
     public static String concatenation(String firstSegment, String secondSegment){
 
-        return null;
+        return firstSegment+secondSegment ;
     }
 
     /**
@@ -28,7 +28,8 @@ public class StringUtilities {
      * @return the concatenation of an integer, `firstSegment`, and a String, `secondSegment`
      */
     public static String concatenation(int firstSegment, String secondSegment){
-        return null;
+
+        return firstSegment+(secondSegment);
     }
 
     /**
@@ -36,7 +37,9 @@ public class StringUtilities {
      * @return the first 3 characters of `input`
      */
     public static String getPrefix(String input){
-        return null;
+
+
+        return input.substring(0,3);
     }
 
     /**
@@ -44,7 +47,8 @@ public class StringUtilities {
      * @return the last 3 characters of `input`
      */
     public static String getSuffix(String input){
-        return null;
+
+        return input.substring(2);
     }
 
     /**
@@ -53,7 +57,9 @@ public class StringUtilities {
      * @return the equivalence of two strings, `inputValue` and `comparableValue`
      */
     public static Boolean compareTwoStrings(String inputValue, String comparableValue){
-        return null;
+
+
+        return inputValue.equals(comparableValue);
     }
 
     /**
@@ -61,15 +67,24 @@ public class StringUtilities {
      * @return the middle character of `inputValue`
      */
     public static Character getMiddleCharacter(String inputValue){
-        return null;
-    }
+
+
+
+            int i = (inputValue.length()-1)/2;
+            char m = inputValue.charAt(i);
+
+            return m;
+        }
+
+
 
     /**
      * @param spaceDelimitedString a string, representative of a sentence, containing spaces
      * @return the first sequence of characters
      */
     public static String getFirstWord(String spaceDelimitedString){
-        return null;
+          String[] arr = spaceDelimitedString.split(" ");
+        return arr[0];
     }
 
     /**
@@ -77,7 +92,9 @@ public class StringUtilities {
      * @return the second word of a string delimited by spaces.
      */
     public static String getSecondWord(String spaceDelimitedString){
-        return null;
+        String[] arr = spaceDelimitedString.split(" ");
+
+        return arr[1];
     }
 
     /**
@@ -85,6 +102,10 @@ public class StringUtilities {
      * @return an identical string with characters in reverse order.
      */
     public static String reverse(String stringToReverse){
-        return null;
+       String reverse = "";
+       for(int i = stringToReverse.length()-1;i>=0;i--){
+           reverse += stringToReverse.charAt(i);
+       }
+        return reverse;
     }
 }
